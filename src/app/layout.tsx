e920f3b5e954f8d0f3a0045599f41119a03fc573
PeterShin23 <pss9fp@virginia.edu>
2025-05-29 19:05:11 -0400

    adding tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Peter Shin",
   description: "Peter Shin's Personal Website",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
