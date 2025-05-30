1fb2aa095bc5065653b24082a1aad4af9de51594
PeterShin23 <pss9fp@virginia.edu>
2025-05-29 23:57:28 -0400

    lint

--- src/components/ParallaxBackground.tsx
+++ src/components/ParallaxBackground.tsx
@@ -4,13 +4,14 @@ import { useScroll, useTransform, motion } from 'framer-motion'
 
 export default function ParallaxBackground() {
   const { scrollY } = useScroll()
-  const y = useTransform(scrollY, [0, 800], [0, -200])
+  const y = useTransform(scrollY, [0, 1100], [0, -250])
 
   return (
     <motion.div
       className="absolute top-0 left-0 w-full h-[200vh] bg-cover bg-center z-0"
       style={{
         backgroundImage: "url('/rain-on-window.jpg')",
+        y
       }}
     >
       <div className="absolute inset-0 bg-gradient-to-b from-black/40 to-transparent backdrop-blur-sm" />
